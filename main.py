7384169dd862176baf46869a003cf056c22979e8
Rafael Rodriguez <rafaelr@familiarodriguez.net>
2026-06-16 23:27:47 -0400

    Step 8 - define new method play

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ class MusicalInstrument:
         self.name = name
         self.instrument_type = instrument_type
 
+    def play(self):
+        pass
+
 instrument_1 = MusicalInstrument("Oboe", "woodwind")
 instrument_2 = MusicalInstrument("Trumpet", "brass")
 
